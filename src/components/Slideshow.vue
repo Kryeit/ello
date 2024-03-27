a1d1d7d05a5ae27c55649991c33b8a0cba6d5714
pipeman <80647470+MrRedRhino@users.noreply.github.com>
2024-03-27 12:23:47 +0100

    Add images and make slideshow actually slide

---
+++ src/components/Slideshow.vue
@@ -0,0 +1,94 @@
+<script setup>
+import {ref} from "vue";
+
+const {images} = defineProps(["images"]);
+
+const currentImage = ref(0);
+const parent = ref();
+let currentTimeout = null;
+
+function deltaImages(n) {
+  let newValue = currentImage.value + n;
+  if (newValue >= images.length) {
+    newValue = 0;
+  } else if (newValue < 0) {
+    newValue = images.length - 1;
+  }
+  currentImage.value = newValue;
+
+  clearTimeout(currentTimeout);
+  // currentTimeout = setTimeout(() => deltaImages(1), 4500);
+}
+
+deltaImages(0);
+</script>
+
+<template>
+  <div ref="parent" class="slideshow">
+    <a class="button" @click="deltaImages(-1)">❮</a>
+    <a class="button right" @click="deltaImages(1)">❯</a>
+    <div :style="{transform: `translateX(${-parent?.clientWidth * currentImage}px)`}" class="image" v-for="image in images">
+      <img :src="image.src" :alt="image.name">
+    </div>
+    <a class="subtitle">{{ images[currentImage].name }}</a>
+  </div>
+</template>
+
+<style scoped>
+.slideshow {
+  position: relative;
+  overflow: hidden;
+  display: flex;
+}
+
+.image {
+  float: left;
+  flex-shrink: 0;
+  width: 100%;
+  transition: transform 1s;
+}
+
+a {
+  color: var(--vt-c-white-mute);
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+.subtitle {
+  left: 0;
+}
+
+.button {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  padding: 16px;
+  margin-top: -22px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  user-select: none;
+  transition: scale 0.2s;
+  height: fit-content;
+  z-index: 10;
+}
+
+.button.right {
+  right: 0;
+}
+
+.button:hover {
+  scale: 1.3;
+}
+
+img {
+  width: 100%;
+  aspect-ratio: 16/9;
+  object-fit: none;
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,68 @@
 <script setup>
+import Slideshow from "@/components/Slideshow.vue";
 
+const images = [
+  {
+    src: "https://kryeit.com/images/img_0.webp",
+    name: "Picture by OlimilO1402"
+  },
+  {
+    src: "https://kryeit.com/images/img_1.webp",
+    name: "Picture by Tesseract"
+  },
+  {
+    src: "https://kryeit.com/images/img_2.webp",
+    name: "Picture by bbqribs2129"
+  },
+  {
+    src: "https://kryeit.com/images/img_3.webp",
+    name: "Picture by Enzoquest10"
+  },
+  {
+    src: "https://kryeit.com/images/img_4.webp",
+    name: "Picture by Enzoquest10"
+  },
+  {
+    src: "https://kryeit.com/images/img_5.webp",
+    name: "Picture by Enzoquest10"
+  },
+  {
+    src: "https://kryeit.com/images/img_6.webp",
+    name: "Picture by Enzoquest10"
+  },
+  {
+    src: "https://kryeit.com/images/img_7.webp",
+    name: "Picture by Derpduck"
+  },
+  {
+    src: "https://kryeit.com/images/img_8.webp",
+    name: "Picture by Enzoquest10"
+  },
+  {
+    src: "https://kryeit.com/images/img_9.webp",
+    name: "Picture by Enzoquest10"
+  },
+  {
+    src: "https://kryeit.com/images/img_10.webp",
+    name: "Picture by Enzoquest10"
+  },
+  {
+    src: "https://kryeit.com/images/img_11.webp",
+    name: "Picture by bbqribs2129"
+  },
+  {
+    src: "https://kryeit.com/images/img_12.webp",
+    name: "Picture by bbqribs2129"
+  },
+  {
+    src: "https://kryeit.com/images/img_13.webp",
+    name: "Picture by Not a Hedgie"
+  },
+  {
+    src: "https://kryeit.com/images/img_14.webp",
+    name: "Picture by OlimilO1402"
+  },
+]
 </script>
 
 <template>
@@ -8,6 +71,18 @@
   </div>
   <h1>This is text. I really like this text. </h1>
   <h2>This is smaller text. I love text.</h2>
+  <h2>Kryeit is a PvE survival Minecraft server focused on the Create mod. We strive for a friendly environment
+    so that everyone can enjoy their time on the server. There is a claim system and a nerfed teleport
+    experience to make travelling easier. The server has a focus on technical minecraft, due to the nature of
+    the Create mod, although all play styles are welcome. The most used form of communication is our Discord
+    server!</h2>
+
+  <Slideshow :images="images"></Slideshow>
+
+  <!--  <iframe src="https://www.youtube.com/embed/Jh2NoGXKzME?autoplay=1&amp;mute=1" title="YouTube video player"-->
+  <!--          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"-->
+  <!--          allowfullscreen="">-->
+  <!--  </iframe>-->
 </template>
 
 <style scoped>
@@ -19,9 +94,15 @@
 
 .banner {
   margin-top: 30px;
-  margin-bottom: 30px ;
+  margin-bottom: 30px;
   display: flex;
   flex-direction: row;
   justify-content: center;
+}
+
+iframe {
+  border: none;
+  width: 100%;
+  aspect-ratio: 16/9;
 }
 </style>

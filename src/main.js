e4fc1a7667d085a81ec1a6faabefb88af0d99cd9
muriplz <46825658+muriplz@users.noreply.github.com>
2024-06-16 00:12:14 +0200

    Clean main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import './assets/styles/main.css'
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
-import ClickOutsideDirective from "@/ClickOutsideDirective.js";
 
 createApp(App)
     .use(router)
-    .directive("click-outside", ClickOutsideDirective)
     .mount('#app');

10dc4b6f8ef7178845a1ce31f21a9973ff6ea159
muriplz <46825658+muriplz@users.noreply.github.com>
2024-07-08 18:06:32 +0200

    Updated footer

--- src/components/navbar/components/Footer.vue
+++ src/components/navbar/components/Footer.vue
@@ -1,9 +1,18 @@
 <script setup>
 import {ref} from "vue";
 import treePath from "@/assets/tree.png";
+import {addToast} from "@/javascript/toasts.js";
 
 const treeSource = ref(treePath);
 
+async function addEmailToast() {
+  addToast('map.png', 'Email copied!', 'Paste this in your email to send us mail.');
+  try {
+    await navigator.clipboard.writeText('kryeit.minecraft@gmail.com');
+  } catch (err) {
+    console.error('Failed to copy email: ', err);
+  }
+}
 </script>
 
 <template>
@@ -11,6 +20,8 @@ const treeSource = ref(treePath);
     <img :src="treeSource" alt=""/>
     <div class="footer-section">
       <p>Kryeit </p>
+      <hr class="separator">
+
       <router-link to="/about">About us</router-link>
       <a href="https://status.kryeit.com" target="_blank">Status</a>
       <a href="https://kryeit.wiki.gg" target="_blank">Wiki</a>
@@ -18,20 +29,25 @@ const treeSource = ref(treePath);
 
     <div class="footer-section">
       <p>Survival</p>
+      <hr class="separator">
+
       <a href="https://map.kryeit.com" target="_blank">World Map</a>
       <a href="https://archive.kryeit.com" target="_blank">Archive</a>
     </div>
 
     <div class="footer-section">
       <p>Support</p>
+      <hr class="separator">
+
       <router-link to="/bans">Bans</router-link>
       <router-link to="/faq">FAQ</router-link>
     </div>
 
     <div class="footer-section">
       <p>Contact</p>
-      <a href="mailto:kryeit.minecraft@gmail.com">Email us</a>
-    </div>
+      <hr class="separator">
+
+      <button @click="addEmailToast">Email us</button>    </div>
 
   </div>
 </template>
@@ -39,16 +55,13 @@ const treeSource = ref(treePath);
 <style scoped>
 .footer {
   bottom: 0;
-  max-height: 17vh;
+  max-height: 30vh;
   width: auto;
   background-color: var(--primary-brass-gold);
   color: white;
-  padding: 0.6rem;
-  text-align: center;
   display: flex;
   justify-content: space-around;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
+  align-items: center;
   gap: 10px;
 }
 
@@ -64,33 +77,38 @@ const treeSource = ref(treePath);
 .footer-section p {
   font-size: 13px;
   color: lightgray;
-  margin-bottom: 7px;
+  margin-bottom: 3px;
 }
 
-.footer-section a {
+.footer-section a, button {
   font-size: 11px;
   white-space: nowrap;
   color: lightblue;
   text-decoration: none;
+  margin-top: 3px;
 }
 
-.footer-section a:hover {
+.footer-section a:hover, button:hover {
   text-decoration: underline;
   color: blue;
 }
 
 .footer img {
-  max-height: 100%;
+  max-height: 80%;
+  height: 130px;
+}
+
+.separator {
+  width: 100%;
 }
 
 @media (max-width: 600px) {
   .footer {
     padding-bottom: 150px;
-    gap: 0;
   }
 
-  .footer-section {
-    padding: 4px;
+  .footer img {
+    display: none;
   }
 }
 </style>
